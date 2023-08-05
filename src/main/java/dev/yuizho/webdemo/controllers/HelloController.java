e2cc347418d451485fb25f6e2b419f263f428c98
yuizho <yuizho3@gmail.com>
2023-08-05 21:34:08 +0900

    modify HelloController

--- src/main/java/dev/yuizho/webdemo/controllers/HelloController.java
+++ src/main/java/dev/yuizho/webdemo/controllers/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("hello")
     public String hello(HttpServletRequest request) {
-        return "Hello!! I'm working on " + request.getLocalName() + "!!!!!!";
+        return "Hello!! I'm working on " + request.getLocalName() + "!!";
     }
 }
