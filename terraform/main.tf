55ef3904232e50b7fb5331ce7343785ab5879b44
yuizho <yuizho3@gmail.com>
2023-10-01 11:55:36 +0900

    modify main.tf to add sg config

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,27 @@ locals {
 resource "aws_instance" "app_server_ec2" {
   ami           = local.instance_ami
   instance_type = local.instance_type
+  vpc_security_group_ids = [aws_security_group.app_server_ec2.id]
 
   tags = {
     Name = "example"
+  }
+}
+
+resource "aws_security_group" "app_server_ec2" {
+  name = "app_server_ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
