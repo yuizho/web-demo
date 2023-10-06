2997c87382e614c29638896c20e2be13a6fbb3e5
yuizho <yuizho3@gmail.com>
2023-10-06 23:18:10 +0900

    refactor terraform configs

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,3 @@
-locals {
-  instance_ami = "ami-0901f015a3d675190"
-}
-
 resource "aws_key_pair" "app_server_ssh_public_key" {
   key_name   = "app_server_ssh_public_key"
   public_key = file(var.public_key_path)
@@ -10,7 +6,7 @@ resource "aws_key_pair" "app_server_ssh_public_key" {
 resource "aws_instance" "app_server_ec2" {
   count = 4
 
-  ami           = local.instance_ami
+  ami           = var.instance_ami
   instance_type = var.instance_type
   key_name = aws_key_pair.app_server_ssh_public_key.id
   vpc_security_group_ids = [
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,11 @@ variable "public_key_path" {
   default = "aws-web-demo.pem.pub"
 }
 
+variable "instance_ami" {
+  type = string
+  default = "ami-0901f015a3d675190"
+}
+
 variable "instance_type" {
   type = string
   default = "t2.micro"
